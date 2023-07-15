719c52aff3bbe7032b05cfe18f87815dbc3a011c
Chris Handy <maximinus@gmail.com>
2023-07-15 16:39:50 +0100

    Preferences widget works

---
+++ MinervaPython/minerva/logs.py
@@ -0,0 +1,4 @@
+import logging
+from pathlib import Path
+
+logging.basicConfig()
--- MinervaPython/minerva/preferences.py
+++ MinervaPython/minerva/preferences.py
@@ -1,3 +1,4 @@
+import json
 import os
 import gi
 
@@ -12,12 +13,39 @@ DEFAULT_CONFIG_FILE = Path('./config/minerva.config')
 
 class Config:
     def __init__(self):
+        self.valid = True
+        self.editor_font = None
+        self.repl_font = None
+        self.lisp_binary = None
+        self.load_config_file()
+
+    def load_config_file(self):
         # load the file, or create if it does not exist
         if not os.path.isfile(DEFAULT_CONFIG_FILE):
             self.create_default_config()
+        try:
+            with open(DEFAULT_CONFIG_FILE, 'r') as read_file:
+                data = json.load(read_file)
+            self.editor_font = data['editor_font']
+            self.repl_font = data['repl_font']
+            self.lisp_binary = data['lisp_binary']
+        except (ValueError, OSError):
+            # could not load the file
+            self.valid = False
 
     def create_default_config(self):
-        pass
+        self.editor_font = None
+        self.repl_font = None
+        self.lisp_binary = '/usr/bin/sbcl'
+        self.update()
+
+    def update(self):
+        data = {'editor_font': self.editor_font,
+                'repl_font': self.repl_font,
+                'lisp_binary': self.lisp_binary}
+        # save the config fie as values have changed
+        with open(DEFAULT_CONFIG_FILE, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
 
 
 class PreferencesDialog:
@@ -32,22 +60,24 @@ class PreferencesDialog:
         self.dialog.run()
         self.dialog.hide()
 
-    def set_editor_font(self, _widget):
-        print(1)
+    def set_editor_font(self, widget):
+        # get the font
+        config.editor_font = widget.get_font_name()
+        config.update()
 
-    def set_repl_font(self, _widget):
-        print(3)
+    def set_repl_font(self, widget):
+        config.repl_font = widget.get_font_name()
+        config.update()
 
-    def config_file_chosen(self, _widget):
-        print(5)
-
-    def lisp_binary_chosen(self, _widget):
-        print(6)
+    def lisp_binary_chosen(self, widget):
+        config.lisp_binary = widget.get_file().get_path()
+        config.update()
 
     def close_dialog(self, _widget):
         self.dialog.response(0)
 
 
+config = Config()
 preferences_dialog = PreferencesDialog()
 
 
