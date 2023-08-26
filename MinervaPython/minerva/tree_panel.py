22357fdef62db9ce5856191ddde1618a829d02b9
Chris Handy <maximinus@gmail.com>
2023-08-26 21:45:24 +0100

    Added gfx. Simple treeview test works

--- MinervaPython/minerva/tree_panel.py
+++ MinervaPython/minerva/tree_panel.py
@@ -52,51 +52,9 @@ class MinvTree(Gtk.Notebook):
         self.show_all()
 
 
-class FileTree(Gtk.ScrolledWindow):
-    def __init__(self):
-        super().__init__()
-        self.store = Gtk.ListStore(str, str, int)
-        #self.store = Gtk.TreeStore()
-        self.get_files()
-        self.treeview = Gtk.TreeView(model=self.store)
-        renderer_1 = Gtk.CellRendererText()
-        column_1 = Gtk.TreeViewColumn('File Name', renderer_1, text=0)
-        # Calling set_sort_column_id makes the treeViewColumn sortable
-        # by clicking on its header. The column is sorted by
-        # the ListStore column index passed to it
-        # (in this case 0 - the first ListStore column)
-        column_1.set_sort_column_id(0)
-        self.treeview.append_column(column_1)
-
-        # xalign=1 right-aligns the file sizes in the second column
-        renderer_2 = Gtk.CellRendererText(xalign=1)
-        # text=1 pulls the data from the second ListStore column
-        # which contains filesizes in bytes formatted as strings
-        # with thousand separators
-        column_2 = Gtk.TreeViewColumn('Size in bytes', renderer_2, text=1)
-        # Mak the Treeview column sortable by the third ListStore column
-        # which contains the actual file sizes
-        column_2.set_sort_column_id(2)
-        self.treeview.append_column(column_2)
-
-        # set up scrolled window
-        self.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
-        self.add(self.treeview)
-
-    def get_files(self):
-        directory = Path(__file__).parent.parent
-        for filename in os.listdir(directory):
-            size = os.path.getsize(os.path.join(directory, filename))
-            # the second element is displayed in the second TreeView column
-            # but that column is sorted by the third element
-            # so the file sizes are sorted as numbers, not as strings
-            self.store.append([filename, f'{size}', size])
-            # if a directory, go down the tree
-
-
 class DirectoryTree:
     def __init__(self, directory):
-        self.dir_path = directory
+        self.dir_path = directory.name
         self.directories = []
         self.files = []
         self.get_directory(directory)
@@ -107,11 +65,40 @@ class DirectoryTree:
             if os.path.isdir(fullpath):
                 self.directories.append(DirectoryTree(fullpath))
             else:
-                self.files.append(fullpath)
+                self.files.append(fullpath.name)
+
+
+class FileTree(Gtk.ScrolledWindow):
+    def __init__(self):
+        super().__init__()
+        self.store = Gtk.TreeStore(str)
+        self.treeview = Gtk.TreeView(model=self.store)
+        renderer_1 = Gtk.CellRendererText()
+        column_1 = Gtk.TreeViewColumn('Filename', renderer_1, text=0)
+        self.treeview.append_column(column_1)
+        # do the calculation at the end
+        get_tree_view(self.store, None, None)
+
+        # set up scrolled window
+        self.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        self.add(self.treeview)
+
+
+def get_tree_view(store, row, new_dir):
+    if new_dir is None:
+        new_dir = DirectoryTree(Path(__file__).parent.parent)
+    for next_dir in new_dir.directories:
+        if row is None:
+            new_row = store.append(None, [next_dir.dir_path])
+        else:
+            new_row = store.append(row, [next_dir.dir_path])
+        get_tree_view(store, new_row, next_dir)
+
+    for file in new_dir.files:
+        store.append(row, [file])
 
 
 if __name__ == '__main__':
-    full_tree = DirectoryTree(Path(__file__).parent.parent)
     # test the file manager on its own
     app = Gtk.Window(title='Treeview Test')
     app.set_default_size(256, 512)
