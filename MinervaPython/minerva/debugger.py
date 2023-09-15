5e70c26683fabc82f18da016820d5ffbeeea6216
Chris Handy <maximinus@gmail.com>
2023-09-15 21:33:16 +0100

    Debugger renders simple stack trace

--- MinervaPython/minerva/debugger.py
+++ MinervaPython/minerva/debugger.py
@@ -17,7 +17,34 @@ EXAMPLE_BUTTONS = ['[CONTINUE] Retry using QWE.',
                    "[*ABORT] Return to SLIME's top level.",
                    '[ABORT] abort thread (#<THREAD "repl-thread" RUNNING {1002A70913}>)']
 
-EXAMPLE_STACK = []
+EXAMPLE_STACK = [
+    ' 0: (SB-INT:SIMPLE-EVAL-IN-LEXENV QWE #<NULL-LEXENV>)',
+    ' 1: (SB-INT:SIMPLE-EVAL-IN-LEXENV (+ 1 QWE) #<NULL-LEXENV>)',
+    ' 2: (EVAL (+ 1 QWE))',
+    ' 3: (SWANK::EVAL-REGION "(+ 1 qwe) ..)',
+    ' 4: ((LAMBDA NIL :IN SWANK-REPL::REPL-EVAL))',
+    ' 5: (SWANK-REPL::TRACK-PACKAGE #<FUNCTION (LAMBDA NIL :IN SWANK-REPL::REPL-EVAL) {1002F5200B}>)',
+    ' 6: (SWANK::CALL-WITH-RETRY-RESTART "Retry SLIME REPL evaluation request." #<FUNCTION (LAMBDA NIL :IN SWANK-REPL::REPL-EVAL) {1002F51FAB}>)',
+    ' 7: (SWANK::CALL-WITH-BUFFER-SYNTAX NIL #<FUNCTION (LAMBDA NIL :IN SWANK-REPL::REPL-EVAL) {1002F51F8B}>)',
+    ' 8: (SWANK-REPL::REPL-EVAL "(+ 1 qwe) ..)',
+    ' 9: (SB-INT:SIMPLE-EVAL-IN-LEXENV (SWANK-REPL:LISTENER-EVAL "(+ 1 qwe) ..)',
+    '10: (EVAL (SWANK-REPL:LISTENER-EVAL "(+ 1 qwe) ..)',
+    '11: (SWANK:EVAL-FOR-EMACS (SWANK-REPL:LISTENER-EVAL "(+ 1 qwe) ..)',
+    '12: (SWANK::PROCESS-REQUESTS NIL)',
+    '13: ((LAMBDA NIL :IN SWANK::HANDLE-REQUESTS))',
+    '14: ((LAMBDA NIL :IN SWANK::HANDLE-REQUESTS))',
+    '15: (SWANK/SBCL::CALL-WITH-BREAK-HOOK #<FUNCTION SWANK:SWANK-DEBUGGER-HOOK> #<FUNCTION (LAMBDA NIL :IN SWANK::HANDLE-REQUESTS) {1002A8C02B}>)',
+    '16: ((FLET SWANK/BACKEND:CALL-WITH-DEBUGGER-HOOK :IN "/home/sparky/.config/emacs/elpa/slime-20230613.1337/swank/sbcl.lisp") #<FUNCTION SWANK:SWANK-DEBUGGER-HOOK> #<FUNCTION (LAMBDA NIL :IN SWANK::HANDLE-R..',
+    '17: (SWANK::CALL-WITH-BINDINGS ((*STANDARD-INPUT* . #<SWANK/GRAY::SLIME-INPUT-STREAM {10028A4B23}>)) #<FUNCTION (LAMBDA NIL :IN SWANK::HANDLE-REQUESTS) {1002A8C04B}>)',
+    '18: (SWANK::HANDLE-REQUESTS #<SWANK::MULTITHREADED-CONNECTION {1001C0A2D3}> NIL)',
+    '19: ((FLET SB-UNIX::BODY :IN SB-THREAD::RUN))',
+    '20: ((FLET "WITHOUT-INTERRUPTS-BODY-10" :IN SB-THREAD::RUN))',
+    '21: ((FLET SB-UNIX::BODY :IN SB-THREAD::RUN))',
+    '22: ((FLET "WITHOUT-INTERRUPTS-BODY-3" :IN SB-THREAD::RUN))',
+    '23: (SB-THREAD::RUN)',
+    '24: ("foreign function: call_into_lisp")',
+    '25: ("foreign function: funcall1")'
+]
 
 
 class DebuggerOptions(Gtk.Box):
@@ -53,25 +80,33 @@ class DebuggerOptions(Gtk.Box):
             self.button_box.pack_start(new_button, False, False, 0)
 
 
+def get_tree_view(store, stack_trace):
+    for i in stack_trace:
+        store.append(None, [i])
+
+
 class DebuggerStack(Gtk.TreeView):
     def __init__(self):
         self.store = Gtk.TreeStore(str)
         super().__init__(model=self.store)
         # design the columns and add them
         column = Gtk.TreeViewColumn('Stack Trace')
-        column.pack_start(Gtk.CellRendererText(), False)
+        cell_text = Gtk.CellRendererText()
+        column.pack_start(cell_text, False)
+        column.add_attribute(cell_text, 'text', 0)
+        get_tree_view(self.store, EXAMPLE_STACK)
         self.append_column(column)
         self.set_margin_top(16)
         self.set_margin_left(16)
         self.set_margin_right(16)
-        self.treeview.set_activate_on_single_click(False)
-        self.treeview.connect('row-activated', self.row_double_click)
-        self.treeview.connect('button-press-event', self.button_press)
+        self.set_activate_on_single_click(False)
+        self.connect('row-activated', self.row_double_click)
+        self.connect('button-press-event', self.button_press)
 
     def row_double_click(self):
         pass
 
-    def button_press(self):
+    def button_press(self, _widget, _row):
         pass
 
 
