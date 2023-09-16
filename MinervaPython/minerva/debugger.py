55351239de65121de328cd50a998a916ec3b5c74
Chris Handy <maximinus@gmail.com>
2023-09-16 23:20:36 +0100

    Buttons are shown on debugger start

--- MinervaPython/minerva/debugger.py
+++ MinervaPython/minerva/debugger.py
@@ -9,45 +9,6 @@ from minerva.helpers.images import get_image
 from minerva.actions import get_named_action, message_queue, Target, Message
 
 
-EXAMPLE_ERROR = '<b>The variable QWE is unbound.\n    [Condition of type UNBOUND-VARIABLE]</b>'
-
-EXAMPLE_BUTTONS = ['[CONTINUE] Retry using QWE.',
-                   '[USE-VALUE] Use specified value.',
-                   '[STORE-VALUE] Set specified value and use it.',
-                   '[RETRY] Retry SLIME REPL evaluation request.',
-                   "[*ABORT] Return to SLIME's top level.",
-                   '[ABORT] abort thread (#<THREAD "repl-thread" RUNNING {1002A70913}>)']
-
-EXAMPLE_STACK = [
-    ' 0: (SB-INT:SIMPLE-EVAL-IN-LEXENV QWE #<NULL-LEXENV>)',
-    ' 1: (SB-INT:SIMPLE-EVAL-IN-LEXENV (+ 1 QWE) #<NULL-LEXENV>)',
-    ' 2: (EVAL (+ 1 QWE))',
-    ' 3: (SWANK::EVAL-REGION "(+ 1 qwe) ..)',
-    ' 4: ((LAMBDA NIL :IN SWANK-REPL::REPL-EVAL))',
-    ' 5: (SWANK-REPL::TRACK-PACKAGE #<FUNCTION (LAMBDA NIL :IN SWANK-REPL::REPL-EVAL) {1002F5200B}>)',
-    ' 6: (SWANK::CALL-WITH-RETRY-RESTART "Retry SLIME REPL evaluation request." #<FUNCTION (LAMBDA NIL :IN SWANK-REPL::REPL-EVAL) {1002F51FAB}>)',
-    ' 7: (SWANK::CALL-WITH-BUFFER-SYNTAX NIL #<FUNCTION (LAMBDA NIL :IN SWANK-REPL::REPL-EVAL) {1002F51F8B}>)',
-    ' 8: (SWANK-REPL::REPL-EVAL "(+ 1 qwe) ..)',
-    ' 9: (SB-INT:SIMPLE-EVAL-IN-LEXENV (SWANK-REPL:LISTENER-EVAL "(+ 1 qwe) ..)',
-    '10: (EVAL (SWANK-REPL:LISTENER-EVAL "(+ 1 qwe) ..)',
-    '11: (SWANK:EVAL-FOR-EMACS (SWANK-REPL:LISTENER-EVAL "(+ 1 qwe) ..)',
-    '12: (SWANK::PROCESS-REQUESTS NIL)',
-    '13: ((LAMBDA NIL :IN SWANK::HANDLE-REQUESTS))',
-    '14: ((LAMBDA NIL :IN SWANK::HANDLE-REQUESTS))',
-    '15: (SWANK/SBCL::CALL-WITH-BREAK-HOOK #<FUNCTION SWANK:SWANK-DEBUGGER-HOOK> #<FUNCTION (LAMBDA NIL :IN SWANK::HANDLE-REQUESTS) {1002A8C02B}>)',
-    '16: ((FLET SWANK/BACKEND:CALL-WITH-DEBUGGER-HOOK :IN "/home/sparky/.config/emacs/elpa/slime-20230613.1337/swank/sbcl.lisp") #<FUNCTION SWANK:SWANK-DEBUGGER-HOOK> #<FUNCTION (LAMBDA NIL :IN SWANK::HANDLE-R..',
-    '17: (SWANK::CALL-WITH-BINDINGS ((*STANDARD-INPUT* . #<SWANK/GRAY::SLIME-INPUT-STREAM {10028A4B23}>)) #<FUNCTION (LAMBDA NIL :IN SWANK::HANDLE-REQUESTS) {1002A8C04B}>)',
-    '18: (SWANK::HANDLE-REQUESTS #<SWANK::MULTITHREADED-CONNECTION {1001C0A2D3}> NIL)',
-    '19: ((FLET SB-UNIX::BODY :IN SB-THREAD::RUN))',
-    '20: ((FLET "WITHOUT-INTERRUPTS-BODY-10" :IN SB-THREAD::RUN))',
-    '21: ((FLET SB-UNIX::BODY :IN SB-THREAD::RUN))',
-    '22: ((FLET "WITHOUT-INTERRUPTS-BODY-3" :IN SB-THREAD::RUN))',
-    '23: (SB-THREAD::RUN)',
-    '24: ("foreign function: call_into_lisp")',
-    '25: ("foreign function: funcall1")'
-]
-
-
 class DebuggerOptions(Gtk.Box):
     def __init__(self):
         super().__init__(orientation=Gtk.Orientation.VERTICAL)
@@ -55,12 +16,10 @@ class DebuggerOptions(Gtk.Box):
         self.set_margin_right(16)
         self.error_label = Gtk.Label()
         self.error_label.set_use_markup(True)
-        self.error_label.set_label(EXAMPLE_ERROR)
         self.error_label.set_halign(Gtk.Align.START)
         self.error_label.set_margin_top(16)
         self.error_label.set_margin_bottom(8)
         self.button_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        self.add_buttons(EXAMPLE_BUTTONS)
         self.pack_start(self.error_label, False, False, 0)
         self.pack_start(self.button_box, False, False, 0)
 
@@ -79,6 +38,7 @@ class DebuggerOptions(Gtk.Box):
             new_button.set_halign(Gtk.Align.FILL)
             new_button.set_alignment(0.0, 0.5)
             self.button_box.pack_start(new_button, False, False, 0)
+        self.button_box.show_all()
 
 
 def get_tree_view(store, stack_trace):
@@ -96,7 +56,7 @@ class DebuggerStack(Gtk.TreeView):
         cell_text = Gtk.CellRendererText()
         column.pack_start(cell_text, False)
         column.add_attribute(cell_text, 'text', 0)
-        get_tree_view(self.store, EXAMPLE_STACK)
+        get_tree_view(self.store, [])
         self.append_column(column)
         self.set_margin_top(16)
         self.set_margin_left(16)
