e456813fef0bdca4ce63e647dafc4a06aa24e225
Chris Handy <maximinus@gmail.com>
2024-08-15 20:29:28 +0100

    Moved to sane file format for tests

--- test/test-widgets.lisp
+++ test/test-widgets.lisp
@@ -10,3 +10,7 @@
   (let ((foo (make-instance 'minerva:ColorRect :size (make-instance 'minerva:Size :width 10 :height 20))))
     (is (and (equal (minerva::width (minerva::min-size foo)) 10)
 	     (equal (minerva::height (minerva::min-size foo)) 20)))))
+
+(test test-no-background-default
+  (let ((foo (make-instance 'minerva:ColorRect :size (make-instance 'minerva:Size :width 10 :height 10) :color '(0 0 0 0))))
+    (is (equal (minerva::background foo) nil))))
